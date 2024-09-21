8a6102731dd4181a3ecf47ff65b1d32c9bcb0ad7
Ihor Panchenko <ihpanchenko@gmail.com>
2024-09-21 23:07:45 +0300

    first commit

---
+++ src/test/java/springweb/courseproject/CourseProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package springweb.courseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
