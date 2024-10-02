df24a02b1570e4f77f152c52463d4945071824e2
Ihor Panchenko <ihpanchenko@gmail.com>
2024-10-02 16:07:30 +0300

    Followed pieces of advice

--- src/main/java/springweb/courseproject/dto/CreateBookRequestDto.java
+++ src/main/java/springweb/courseproject/dto/CreateBookRequestDto.java
@@ -5,14 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
-import springweb.courseproject.validation.Author;
 import springweb.courseproject.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
     @NotBlank
     private String title;
-    @Author
+    @NotBlank
     private String author;
     @Isbn
     private String isbn;
