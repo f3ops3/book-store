b91eca4a41c8e75b973a0fa1c6c1e81ebdba9a23
Ihor Panchenko <ihpanchenko@gmail.com>
2024-10-02 19:31:04 +0300

    Added pagination with sorting and swagger.

--- src/main/java/springweb/courseproject/controller/BookController.java
+++ src/main/java/springweb/courseproject/controller/BookController.java
@@ -1,8 +1,11 @@
 package springweb.courseproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,40 +19,47 @@ import springweb.courseproject.dto.BookSearchParametersDto;
 import springweb.courseproject.dto.CreateBookRequestDto;
 import springweb.courseproject.service.BookService;
 
+@Tag(name = "Books store", description = "Endpoints for managing books")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Get all books")
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Create a new book", description = "Create a new book")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
+    @Operation(summary = "Get a book", description = "Get a particular book by id")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
+    @Operation(summary = "Delete a book", description = "Delete a particular book by id")
     @DeleteMapping("/{id}")
     public void deleteBookById(@PathVariable Long id) {
         bookService.deleteBookById(id);
     }
 
+    @Operation(summary = "Update a book", description = "Update a particular book by id")
     @PutMapping("/{id}")
     public BookDto updateBook(@RequestBody @Valid CreateBookRequestDto bookDto,
                               @PathVariable Long id) {
         return bookService.updateBookById(id, bookDto);
     }
 
+    @Operation(summary = "Search books", description = "Search books by parameters")
     @GetMapping("/search")
-    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
-        return bookService.search(searchParameters);
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters, Pageable pageable) {
+        return bookService.search(searchParameters, pageable);
     }
 }
--- src/main/java/springweb/courseproject/service/BookServiceImpl.java
+++ src/main/java/springweb/courseproject/service/BookServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import springweb.courseproject.dto.BookDto;
@@ -28,8 +29,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
@@ -56,11 +57,12 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> search(BookSearchParametersDto searchParametersDto) {
-        Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParametersDto);
-        return bookRepository.findAll(bookSpecification)
-                .stream()
+    public List<BookDto> search(BookSearchParametersDto searchParametersDto, Pageable pageable) {
+        Specification<Book> bookSpecification = bookSpecificationBuilder
+                .build(searchParametersDto);
+        return bookRepository.findAll(bookSpecification, pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
+
 }
